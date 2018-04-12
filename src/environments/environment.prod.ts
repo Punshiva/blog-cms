b484acac2c1b01f13a346cb5208b0b34a4f93b65
Shiva Pun <sp1391@live.mdx.ac.uk>
2018-04-12 17:07:56 +0100

    Add lessons list getting functionality

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase:{
+    apiKey: "AIzaSyA5suo91mYFspY1IZ3vqVPbSnFIoqXmafo",
+    authDomain: "my-blog-bf16e.firebaseapp.com",
+    databaseURL: "https://my-blog-bf16e.firebaseio.com",
+    projectId: "my-blog-bf16e",
+    storageBucket: "my-blog-bf16e.appspot.com",
+    messagingSenderId: "694076320155"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,5 +4,13 @@
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase:{
+    apiKey: "AIzaSyA5suo91mYFspY1IZ3vqVPbSnFIoqXmafo",
+    authDomain: "my-blog-bf16e.firebaseapp.com",
+    databaseURL: "https://my-blog-bf16e.firebaseio.com",
+    projectId: "my-blog-bf16e",
+    storageBucket: "my-blog-bf16e.appspot.com",
+    messagingSenderId: "694076320155"
+  }
 };
